7eedf0ccb392ff1619b659ba685850376adb775c
Greg Ellson <gellson@inkbit3d.com>
2023-02-11 08:04:20 -0500

    get and has pass tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
-use std::path::PathBuf;
 use std::fmt;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::PathBuf;
 
 use directories::ProjectDirs;
 use jsonschema::JSONSchema;
 use serde_json::Value;
 use thiserror::Error;
 
-fn defaultConfig(schema: &JSONSchema) -> Value {
-
+fn default_config(schema: &JSONSchema) -> Value {
+    return Value::from("value");
 }
 
 #[derive(Error, Debug)]
@@ -26,7 +26,7 @@ pub enum StoreError {
 pub struct Store {
     path: PathBuf,
     schema: Option<JSONSchema>,
-    config: Value
+    config: Value,
 }
 
 impl fmt::Display for Store {
@@ -36,19 +36,34 @@ impl fmt::Display for Store {
 }
 
 impl Store {
-    pub fn new(company_name: String, app_name: String, schema_path: Option<PathBuf>, path_override: Option<PathBuf>) -> Result<Store, StoreError> {
-
+    pub fn new(
+        company_name: String,
+        app_name: String,
+        schema_path: Option<PathBuf>,
+        path_override: Option<PathBuf>,
+    ) -> Result<Store, StoreError> {
         // Initialize schema as None, then load if a path was provided
         let mut schema: Option<JSONSchema> = None;
         if let Some(schema_path) = schema_path {
-            schema = Some(JSONSchema::compile(&serde_json::from_reader(BufReader::new(File::open(&schema_path).expect("Failed to open file"))).unwrap()).unwrap());
+            schema = Some(
+                JSONSchema::compile(
+                    &serde_json::from_reader(BufReader::new(
+                        File::open(&schema_path).expect("Failed to open file"),
+                    ))
+                    .unwrap(),
+                )
+                .unwrap(),
+            );
         }
+
+        let mut config_path: PathBuf = PathBuf::new();
 
         // If a path override was provided, use that for config path
         if let Some(path) = path_override {
             // make sure the path is a directory that exists
             if path.is_dir() && path.exists() {
-                let config_path = path.join("config.json");
+                config_path = path.join("config.json");
+                
             } else {
                 panic!("invalid override path");
             }
@@ -56,35 +71,54 @@ impl Store {
             // See if a config json file exists in the UserData directory for the provided app name
             // Get the config directory
             if let Some(proj_dirs) = ProjectDirs::from("com", &company_name, &app_name) {
-                let config_path = PathBuf::from(proj_dirs.config_dir()).join("config.json");
-
-                // If the file exists, load it
-                if config_path.exists() {
-                    let mut config: Value = serde_json::from_reader(BufReader::new(File::open(&config_path).expect("Failed to open file"))).unwrap();
-                    // Validate the config against the schema
-                    if let Some(s) = &schema {
-                        // if the config passes validation, return the Store
-                        if s.is_valid(&config) {
-                            return Ok(Store { path: config_path, config, schema });
-                        } else { // otherwise, generate a default config and return a store
-                            config = defaultConfig(s);
-                            return Ok(Store {path: config_path, config, schema});
-                        }
-                    } else { // if no schema, just return the store
-                        return Ok(Store {path: config_path, config, schema});
-                    }
-                } else if let Some(s) = &schema { // if no config exists but there is a store, create a default config and return the store
-                    let config: Value = defaultConfig(s);
-                    return Ok(Store {path: config_path, config, schema});
-                } else { // if there is no config and no schema, panic?
-                    panic!("No config found and no schema provided");
-                }
+                config_path = PathBuf::from(proj_dirs.config_dir()).join("config.json");
             }
         }
 
-        
-
-        return Err(StoreError::InitError);
+        // If the file exists, load it
+        if config_path.exists() {
+            let mut config: Value = serde_json::from_reader(BufReader::new(
+                File::open(&config_path).expect("Failed to open file"),
+            ))
+            .unwrap();
+            // Validate the config against the schema
+            if let Some(s) = &schema {
+                // if the config passes validation, return the Store
+                if s.is_valid(&config) {
+                    return Ok(Store {
+                        path: config_path,
+                        config,
+                        schema,
+                    });
+                } else {
+                    // otherwise, generate a default config and return a store
+                    config = default_config(s);
+                    return Ok(Store {
+                        path: config_path,
+                        config,
+                        schema,
+                    });
+                }
+            } else {
+                // if no schema, just return the store
+                return Ok(Store {
+                    path: config_path,
+                    config,
+                    schema,
+                });
+            }
+        } else if let Some(s) = &schema {
+            // if no config exists but there is a schema, create a default config and return the store
+            let config: Value = default_config(s);
+            return Ok(Store {
+                path: config_path,
+                config,
+                schema,
+            });
+        } else {
+            // if there is no config and no schema, error
+            return Err(StoreError::InitError);
+        }
     }
 
     // Get a value from the store, or the default if it doesn't exist, or error if it isn't a valid key
@@ -97,7 +131,7 @@ impl Store {
                 return Err(StoreError::InvalidKey);
             }
         }
-        
+
         return Ok(current_value.clone());
     }
 
@@ -135,10 +169,9 @@ impl Store {
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
-
     use super::*;
 
+    // tests basic store initialization
     #[test]
     fn init_store() {
         let company_name: &str = "ACME";
@@ -149,15 +182,51 @@ mod tests {
             panic!("Failed to initialize store")
         }
     }
+    // tests schema validation
     #[test]
     fn test_validate_schema() {
         let schema_path = PathBuf::from("tests/config.schema.json");
-        let schema: Value = serde_json::from_reader(BufReader::new(File::open(&schema_path).expect("Failed to open file"))).unwrap();
+        let schema: Value = serde_json::from_reader(BufReader::new(
+            File::open(&schema_path).expect("Failed to open file"),
+        ))
+        .unwrap();
         let compiled_schema = JSONSchema::compile(&schema).expect("Failed to compile schema");
 
         let config_path = PathBuf::from("tests/config.json");
-        let config: Value = serde_json::from_reader(BufReader::new(File::open(&config_path).expect("Failed to open file"))).unwrap();
+        let config: Value = serde_json::from_reader(BufReader::new(
+            File::open(&config_path).expect("Failed to open file"),
+        ))
+        .unwrap();
 
         assert!(compiled_schema.is_valid(&config));
+    }
+    // tests if it can get a value from the store
+    #[test]
+    fn test_get() {
+        let store = Store::new(
+            String::from("ACME"),
+            String::from("Dynamite"),
+            Some(PathBuf::from("tests/config.schema.json")),
+            Some(PathBuf::from("tests/")),
+        )
+        .expect("failed to load test config");
+        let input = String::from("aSetting.i");
+        assert_eq!(store.get(input).unwrap(), 400);
+    }
+    // test check if value is present
+    #[test]
+    fn test_has() {
+        let store = Store::new(
+            String::from("ACME"),
+            String::from("Dynamite"),
+            Some(PathBuf::from("tests/config.schema.json")),
+            Some(PathBuf::from("tests/")),
+        )
+        .expect("failed to load test config");
+
+        let input_true = String::from("anotherSetting.y");
+        assert!(store.has(input_true));
+        let input_false = String::from("not.here");
+        assert!(!store.has(input_false));
     }
 }
